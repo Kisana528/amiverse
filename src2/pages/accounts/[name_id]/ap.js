ffdb5cf5f49f86aa8b6bcfcea6f0d5808da2b9f0
kisana <yuto01200120yuto@gmail.com>
2023-09-10 21:54:43 +0900

    [FIX]ap

--- src2/pages/accounts/[name_id]/ap.js
+++ src2/pages/accounts/[name_id]/ap.js
@@ -14,7 +14,27 @@ export async function getServerSideProps({req, res, context, query}) {
   const data = {
     "@context": [
       "https://www.w3.org/ns/activitystreams",
-      "https://w3id.org/security/v1"
+      "https://w3id.org/security/v1",
+      {
+        "manuallyApprovesFollowers": "as:manuallyApprovesFollowers",
+        "sensitive": "as:sensitive",
+        "Hashtag": "as:Hashtag",
+        "quoteUrl": "as:quoteUrl",
+        "toot": "http://joinmastodon.org/ns#",
+        "Emoji": "toot:Emoji",
+        "featured": "toot:featured",
+        "discoverable": "toot:discoverable",
+        "schema": "http://schema.org#",
+        "PropertyValue": "schema:PropertyValue",
+        "value": "schema:value",
+        "misskey": "https://misskey-hub.net/ns#",
+        "_misskey_content": "misskey:_misskey_content",
+        "_misskey_quote": "misskey:_misskey_quote",
+        "_misskey_reaction": "misskey:_misskey_reaction",
+        "_misskey_votes": "misskey:_misskey_votes",
+        "isCat": "misskey:isCat",
+        "vcard": "http://www.w3.org/2006/vcard/ns#"
+      }
     ],
     type: "Person",
     id: FullAppUrl(`@${accountData.name_id}`),
