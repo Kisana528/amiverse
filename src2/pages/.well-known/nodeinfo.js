be40367f49253d52480bef53cc494e808f02e6a0
kisana <yuto01200120yuto@gmail.com>
2023-11-21 13:45:53 +0900

    [ADD]nodeinfo2.1&outbox

--- src2/pages/.well-known/nodeinfo.js
+++ src2/pages/.well-known/nodeinfo.js
@@ -6,7 +6,12 @@ export async function getServerSideProps({ req, res, context, query }) {
     "links":[{
       "rel":"http://nodeinfo.diaspora.software/ns/schema/2.0",
       "href":"https://amiverse.net/nodeinfo/2.0"
-    }]
+    },
+    {
+      "rel":"http://nodeinfo.diaspora.software/ns/schema/2.1",
+      "href":"https://amiverse.net/nodeinfo/2.1"
+    }
+  ]
   }
   res.setHeader('Content-Type', 'application/json')
   res.write(JSON.stringify(data))
--- src2/pages/accounts/[name_id]/outbox.js
+++ src2/pages/accounts/[name_id]/outbox.js
@@ -1,10 +1,48 @@
+import axios from '@/lib/axios'
+
 export async function getServerSideProps({req, res, context, query}) {
+  let accountData
+  if (query.name_id) {
+    await axios.post('http://app:3000/v1/@' + query.name_id)
+      .then(res => {
+        accountData = res.data
+        console.log(accountData.items.length)
+      })
+      .catch(err => {
+        //アカウント取得例外
+      })
+  }
+
   const data = {
     "@context": "https://www.w3.org/ns/activitystreams",
     "summary": "outbox desu.",
     "type": "OrderedCollection",
-    "totalItems": 0,
-    "orderedItems": []
+    "totalItems": 1,
+    "orderedItems": 
+      accountData.items.map(item => ({
+        "@context": "https://www.w3.org/ns/activitystreams",
+        "type": "Create",
+        "id": `https://amiverse.net/items/${item.item_id}`,
+        "url": `https://amiverse.net/items/${item.item_id}`,
+        "published": item.created_at,
+        "to": [
+          `https://amiverse.net/${accountData.name_id}/followers`,
+          "https://www.w3.org/ns/activitystreams#Public"
+        ],
+        "actor": `https://amiverse.net/${accountData.name_id}`,
+        "object": {
+          "@context": "https://www.w3.org/ns/activitystreams",
+          "type": "Note",
+          "id": `https://amiverse.net/items/${item.item_id}`,
+          "url": `https://amiverse.net/items/${item.item_id}`,          "published": "2023-01-16T06:48:29Z",
+          "to": [
+            `https://amiverse.net/${accountData.name_id}/followers`,
+            "https://www.w3.org/ns/activitystreams#Public"
+          ],
+          "attributedTo": `https://amiverse.net/${accountData.name_id}/followers`,
+          "content": item.content
+        }
+      }))
   }
 
   res.setHeader('Content-Type', 'application/activity+json; charset=utf-8')
