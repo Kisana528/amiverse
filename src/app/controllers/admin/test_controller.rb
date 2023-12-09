0b82aa549c2f1b855a8f165bb8a69da11b2005a6
kisana <yuto01200120yuto@gmail.com>
2023-12-09 23:18:22 +0900

    Revert "[CHANGE]digestが違ったかも"
    This reverts commit 25b09ef9fbd8f9ae577de4939a54627495a733eb.

--- src/app/controllers/admin/test_controller.rb
+++ src/app/controllers/admin/test_controller.rb
@@ -2,12 +2,9 @@ class Admin::TestController < Admin::ApplicationController
   def new
   end
   def verify
-    #result = verify_signature(params[:public_key], params[:signature], params[:message])
+    result = verify_signature(params[:public_key], params[:signature], params[:message])
     Rails.logger.info('-----結果発表------')
-    Rails.logger.info(Digest::SHA256.digest(params[:message]))
-    Rails.logger.info(Digest::SHA256.hexdigest(params[:message]))
-    Rails.logger.info(Digest::SHA256.base64digest(params[:message]))
-    #Rails.logger.info(result ? 'ok' : 'ng')
+    Rails.logger.info(result ? 'ok' : 'ng')
     render 'index'
   end
   private
